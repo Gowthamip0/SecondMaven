ed7f9075a03aa2a057d35e83d4089e1a92926607
Gowthamip0 <gowthamipulicherla83@gmail.com>
2019-03-02 00:26:57 +0530

    success installation

--- com.secondMaven/src/test/java/com/secondMaven/com/secondMaven/TestJenkins.java
+++ com.secondMaven/src/test/java/com/secondMaven/com/secondMaven/TestJenkins.java
@@ -2,7 +2,6 @@ package com.secondMaven.com.secondMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestJenkins {

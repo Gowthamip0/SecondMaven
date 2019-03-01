580a0c849542ad6aaecc6db160fda86adb0f3293
Gowthamip0 <gowthamipulicherla83@gmail.com>
2019-03-02 00:02:40 +0530

    test case added

--- com.secondMaven/src/test/java/com/secondMaven/com/secondMaven/TestJenkins.java
+++ com.secondMaven/src/test/java/com/secondMaven/com/secondMaven/TestJenkins.java
@@ -2,11 +2,14 @@ package com.secondMaven.com.secondMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-public class Test {
+public class TestJenkins {
 
-	public static void main(String[] args) {
-		
+	@Test
+	public void launch()
+	{
 		System.setProperty("webdriver.chrome.driver", "C://Users//Gowthami Pulicherla//Documents//chromedriverlatest//chromedriver_win32//chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.softwaretestingmaterial.com/selenium-continuous-integration/");

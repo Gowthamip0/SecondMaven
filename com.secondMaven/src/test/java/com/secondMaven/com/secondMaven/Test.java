86f2a1223436d154844eb20fecdd2504323ae74c
Gowthamip0 <gowthamipulicherla83@gmail.com>
2019-03-01 23:20:23 +0530

    Launching browser

--- com.secondMaven/src/test/java/com/secondMaven/com/secondMaven/Test.java
+++ com.secondMaven/src/test/java/com/secondMaven/com/secondMaven/Test.java
@@ -1,8 +1,16 @@
 package com.secondMaven.com.secondMaven;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C://Users//Gowthami Pulicherla//Documents//chromedriverlatest//chromedriver_win32//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.softwaretestingmaterial.com/selenium-continuous-integration/");
+		
 		
 		
 		
